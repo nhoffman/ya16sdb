8110c3b4fd1dd6d0e0fda606360327aa5a2a104f
Chris Rosenthal <crosenth@uw.edu>
2014-03-18 15:50:26 -0700

    grabbing virtualenv to install requirements.txt

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [[ -z $1 ]]; then
+    venv=$(basename $(pwd))-env
+else
+    venv=$1
+fi
+
 MKREFPKG_DIR=$(cd $(dirname $BASH_SOURCE) && cd .. && pwd)
 DEENURP_DIR=${MKREFPKG_DIR}/src/deenurp
 
@@ -9,6 +15,8 @@ if [ ! -d $DEENURP_DIR ]; then
   git clone git@github.com:fhcrc/deenurp.git $DEENURP_DIR
 fi
 
-${DEENURP_DIR}/bin/bootstrap.sh
+${DEENURP_DIR}/bin/bootstrap.sh $venv
+
+source $venv/bin/activate
 
 pip install -U -r ${MKREFPKG_DIR}/requirements.txt
