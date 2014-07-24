c077cc367becb940d63fe1c90476944d779832b2
Noah Hoffman <noah.hoffman@gmail.com>
2014-07-24 13:28:20 -0700

    bootstrap script can install from wheels

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# set up a virtualenv
+
+# `DEENURP_BRANCH` optionally defines the git branch to check out from
+# the deenurp repository (default master)
+
+# `WHEELHOUSE` optionally provides a path or url containing python
+# wheels for `pip install --find-links=...`
+
 set -e
 
 if [[ -z $1 ]]; then
@@ -9,15 +17,26 @@ else
 fi
 
 MKREFPKG_DIR=$(cd $(dirname $BASH_SOURCE) && cd .. && pwd)
-DEENURP_DIR=src/deenurp
+DEENURP=src/deenurp
 mkdir -p src
 
-if [ ! -d $DEENURP_DIR ]; then
-    git clone git@github.com:fhcrc/deenurp.git $DEENURP_DIR
+if [[ -z "$DEENURP_BRANCH" ]]; then
+    DEENURP_BRANCH=master
 fi
 
-${DEENURP_DIR}/bin/bootstrap.sh $venv
+if [ ! -d $DEENURP ]; then
+    git clone -b "$DEENURP_BRANCH" git@github.com:fhcrc/deenurp.git $DEENURP
+fi
+
+${DEENURP}/bin/bootstrap.sh $venv
 
 source $venv/bin/activate
 
-pip install -U -r ${MKREFPKG_DIR}/requirements.txt
+if [[ -z "$WHEELHOUSE" ]]; then
+    pip install -U -r ${MKREFPKG_DIR}/requirements.txt
+else
+    pip install -U --use-wheel --find-links="$WHEELHOUSE" -r ${MKREFPKG_DIR}/requirements.txt
+fi
+
+
+
