9f0c39af95911ec487b7bed4737d931bb10fae01
Chris Rosenthal <crosenth@uw.edu>
2015-08-10 15:44:21 -0700

    Using PIP_FIND_LINKS to install wheels https://pip.pypa.io/en/latest/user_guide.html?#environment-variables

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -5,8 +5,8 @@
 # `DEENURP_BRANCH` optionally defines the git branch to check out from
 # the deenurp repository (default master)
 
-# `WHEELHOUSE` optionally provides a path or url containing python
-# wheels for `pip install --find-links=...`
+# `PIP_FIND_LINKES` optionally provides a path or url containing python
+# wheels https://pip.pypa.io/en/latest/user_guide.html#environment-variables
 
 set -e
 
@@ -32,11 +32,7 @@ ${DEENURP}/bin/bootstrap.sh $venv
 
 source $venv/bin/activate
 
-if [[ -z "$WHEELHOUSE" ]]; then
-    pip install -U -r ${MKREFPKG_DIR}/requirements.txt
-else
-    pip install -U --use-wheel --find-links="$WHEELHOUSE" -r ${MKREFPKG_DIR}/requirements.txt
-fi
+# set PIP_FIND_LINKES to use wheels https://pip.pypa.io/en/latest/user_guide.html#environment-variables
+pip install --upgrade --requirement ${MKREFPKG_DIR}/requirements.txt
 
-
-
+virtualenv --relocatable $venv
