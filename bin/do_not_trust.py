3f6ea661b833c4994f2d8cbd05ec60eaa61c99cc
Chris Rosenthal <crosenth@uw.edu>
2018-01-09 10:39:23 -0800

    ignore comments and blank lines

--- bin/do_not_trust.py
+++ bin/do_not_trust.py
@@ -15,9 +15,10 @@ def main():
         formatter_class=argparse.RawDescriptionHelpFormatter)
 
     p.add_argument('do_not_trust', type=argparse.FileType('r'))
+    p.add_argument('annotations', type=argparse.FileType('r'))
 
     p.add_argument('fasta')
-    p.add_argument('annotations', type=argparse.FileType('r'))
+    p.add_argument('seq_info', type=argparse.FileType('r'))
     p.add_argument('details', type=argparse.FileType('r'))
 
     p.add_argument('out_fa', type=argparse.FileType('w'))
@@ -26,23 +27,25 @@ def main():
 
     args = p.parse_args()
 
-    do_not_trust = set(args.do_not_trust)
+    do_not_trust = (a.strip() for a in args.do_not_trust)
+    do_not_trust = (a for a in do_not_trust if a)
+    do_not_trust = (a for a in do_not_trust if not a.startswith('#'))
+    do_not_trust = set(do_not_trust)
 
     keep = []
-
-    annos = csv.DictReader(args.annotations)
-    out_annos = csv.DictWriter(args.out_info, annos.fieldnames)
-    out_annos.writeheader()
-    for row in annos:
+    for row in csv.DictReader(args.annotations):
         if row['accession'] not in do_not_trust:
-            out_annos.writerow(row)
             keep.append(row['seqname'])
-
     keep = set(keep)
 
     for seq in SeqIO.parse(args.fasta, 'fasta'):
         if seq.id in keep:
             args.out_fa.write('>{}\n{}\n'.format(seq.description, seq.seq))
+
+    seq_info = csv.DictReader(args.seq_info)
+    out_info = csv.DictWriter(args.out_info, seq_info.fieldnames)
+    out_info.writeheader()
+    out_info.writerows(i for i in seq_info if i['seqname'] in keep)
 
     details = csv.DictReader(args.details)
     out_details = csv.DictWriter(args.out_details, details.fieldnames)
