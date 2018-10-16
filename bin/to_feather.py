5b3d12988d5af8d4ff2998974852a023e413357c
Chris Rosenthal <crosenth@uw.edu>
2018-10-16 16:22:24 -0700

    feather file adds datetime dtype and genus and species tax_names plus pubmed_ids

--- bin/to_feather.py
+++ bin/to_feather.py
@@ -1,89 +1,125 @@
 #!/usr/bin/env python
-
 """
-TODO: use taxit to calculae lineages here or do it in pipeline
-
-eg:
-
-datadir=/mnt/disk2/molmicro/common/ncbi/16s/output/20180214
-singularity exec -B $datadir --pwd $datadir/dedup/1200bp/named /molmicro/common/singularity/taxtastic-0.8.5-singularity2.4.img taxit lineage_table taxonomy.csv seq_info.csv -c lineages.csv
-
-Then:
-
-export named=$datadir/dedup/1200bp/named
-bin/feather.py \
-    --details $named/filtered/details_out.csv \
-    --seq-info $named/seq_info.csv \
-    --lineages $named/lineages.csv \
-    --hits $named/vsearch.tsv \
-    -o $named/filtered_details.feather
+Create a feather file combining sequence details,
+seq_info, taxonomy and publication annotations
 """
-
-import sys
-from os import path
 import argparse
+import pandas
+import sys
 
-import pandas as pd
+DTYPES = {
+    'accession': str,
+    'ambig_count': int,
+    'authors': str,
+    'centroid': str,
+    'cluster': float,
+    'comment': str,
+    'consrtm': str,
+    'description': str,
+    'dist': float,
+    'download_date': str,
+    'genus': str,
+    'is_out': bool,
+    'is_type': bool,
+    'isolate': str,
+    'isolation_source': str,
+    'journal': str,
+    'keywords': str,
+    'length': int,
+    'match_pct': float,
+    'match_seqname': str,
+    'modified_date': str,
+    'mol_type': str,
+    'name': str,
+    'organism': str,
+    'pubmed_id': str,
+    'seq_start': int,
+    'seq_stop': int,
+    'seqname': str,
+    'source': str,
+    'species': str,
+    'strain': str,
+    'tax_id': str,
+    'title': str,
+    'version': str,
+    'version_num': str,
+    'x': float,
+    'y': float,
+    }
+
 
 def main(arguments):
-
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--details', help='[default %(default)s]', default='details_out.csv')
-    parser.add_argument('--seq-info', help='[default %(default)s]', default='seq_info.csv')
-    parser.add_argument('--lineages', help='[default %(default)s]', default='lineages.csv')
-    parser.add_argument('--hits', help='[default %(default)s]', default='vsearch.tsv')
-    parser.add_argument('-o', '--outfile', help='[default %(default)s]', default='filtered_details.feather')
-
+    parser.add_argument('details')
+    parser.add_argument('seq_info')
+    parser.add_argument('taxonomy')
+    parser.add_argument('hits')
+    parser.add_argument('pubmed_ids')
+    parser.add_argument('--out', default=sys.stdout, help='[%(default)s]', )
     args = parser.parse_args(arguments)
 
-    outfile = args.outfile
-
-    details = pd.read_csv(
-        args.details,
-        dtype={
-            'cluster': str,
-            'tax_id': str,
-            'species': str,
-            'is_out': str,
-        })
-
-    seq_info = pd.read_csv(
+    details = pandas.read_csv(args.details, dtype=DTYPES)
+    seq_info = pandas.read_csv(
         args.seq_info,
-        usecols=['seqname', 'accession', 'description', 'is_type'],
-    )
-
-    details = pd.merge(details, seq_info, how='left', on=['seqname'])
-
-    lineages = pd.read_csv(
-        args.lineages,
-        usecols=['seqname', 'species'],
-    )
-    lineages = lineages.rename(columns={'species': 'tax_name'})
-
-    hits = pd.read_table(
+        dtype=DTYPES,
+        parse_dates=['download_date', 'modified_date'],
+        date_parser=lambda x: pandas.datetime.strptime(x, '%d-%b-%Y'))
+    taxonomy = pandas.read_csv(
+        args.taxonomy,
+        dtype=DTYPES,
+        usecols=['tax_id', 'tax_name', 'species', 'genus'])
+    hits = pandas.read_table(
         args.hits,
-        usecols=[0, 1, 2],
+        dtype=DTYPES,
         header=None,
         names=['seqname', 'match_seqname', 'match_pct'],
-    )
-    hits = pd.merge(
-        hits, lineages.rename(
-            columns={'seqname': 'match_seqname', 'tax_name': 'match_species'}
-        ),
-        how='left', on=['match_seqname']
-    )
+        usecols=['seqname', 'match_seqname', 'match_pct'])
+    pubmed_ids = pandas.read_csv(
+        args.pubmed_ids, dtype=DTYPES, usecols=['pubmed_id', 'version'])
 
-    df = pd.merge(details, lineages, how='left', on=['seqname'])
-    df = pd.merge(df, hits, how='left', on=['seqname'])
+    hits = hits.merge(
+        seq_info[['seqname', 'tax_id']],
+        left_on='match_seqname',
+        suffixes=['', '_'],
+        right_on='seqname').drop(columns='seqname_', axis='columns')
+    hits = hits.merge(taxonomy[['tax_id', 'species']].rename(
+        columns={'species': 'match_species'}))
+    hits = hits.merge(taxonomy[['tax_id', 'tax_name']].rename(
+        columns={'tax_name': 'match_name'}),
+        left_on='match_species',
+        right_on='tax_id',
+        suffixes=['', '_']).drop(columns='tax_id_', axis='columns')
 
-    print(df.head(1))
-    print(pd.isnull(df).describe())
-    df.to_feather(outfile)
+    before_merging = len(details)
+    details = details.merge(seq_info)
+    details = details.merge(taxonomy)
+    details = details.merge(hits, how='left')
+    details = details.merge(pubmed_ids, how='left')
+    assert(len(details) == before_merging)
+
+    taxonomy = taxonomy[['tax_id', 'tax_name']]
+
+    details = details.merge(
+        taxonomy.rename(columns={'tax_name': 'genus_name'}),
+        how='left',
+        left_on='genus',
+        right_on='tax_id',
+        suffixes=('', '_')).drop(columns='tax_id_', axis='columns')
+    details = details.merge(
+        taxonomy.rename(columns={'tax_name': 'species_name'}),
+        left_on='species',
+        right_on='tax_id',
+        suffixes=('', '_')).drop(columns='tax_id_', axis='columns')
+
+    # setup data for dist v rank_order plot
+    details = details.sort_values(by='dist')
+    species_groups = details.groupby(by='species')['species']
+    details['rank_order'] = species_groups.transform(lambda x: range(len(x)))
+
+    details.reset_index(drop=True).to_feather(args.out)
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
-
-# read it back in:
-# time python -c "import pandas as pd; df = pd.read_feather('output/filtered_details.feather'); print(df.head())"
