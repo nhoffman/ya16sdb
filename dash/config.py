b874ed2df76e6f245ef8b68b5e8f1c9c93144917
Chris Rosenthal <crosenth@uw.edu>
2018-10-23 15:42:39 -0700

    adding Docker notes for Docker settings

--- dash/config.py
+++ dash/config.py
@@ -1,8 +1,6 @@
-import multiprocessing
-
 bind = "127.0.0.1:8005"  # 0.0.0.0:443  # Docker
 certfile = 'test.crt'
-# chdir = '/dash'
+# chdir = '/dash'  # docker
 keyfile = 'test.key'
 threads = 2
 workers = 2
