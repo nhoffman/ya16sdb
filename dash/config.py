6856214967d117a65dbbf0e3b3e909878f27af1c
Chris Rosenthal <crosenth@uw.edu>
2018-10-24 15:26:52 -0700

    adding count_cpus() for cloud use

--- dash/config.py
+++ dash/config.py
@@ -1,6 +1,8 @@
+import multiprocessing
+
 bind = "127.0.0.1:8005"  # 0.0.0.0:443  # Docker
 certfile = 'test.crt'
 # chdir = '/dash'  # docker
 keyfile = 'test.key'
-threads = 2
-workers = 2
+threads = 4
+workers = 4  # multiprocessing.cpu_count() * 2 + 1
