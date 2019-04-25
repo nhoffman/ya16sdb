60e925a68a0aa370cf201d080dfa530aa372c2e4
crosenth <crosenth@uw.edu>
2019-04-25 13:42:34 -0700

    merges in the is_valid columns from the taxonomy database into feather file

--- bin/is_valid.py
+++ bin/is_valid.py
@@ -5,6 +5,8 @@ output all valid tax_ids in database
 
 import argparse
 import configparser
+import hashlib
+import pandas
 import sqlalchemy
 import sys
 
@@ -14,14 +16,17 @@ def main():
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     p.add_argument(
+        'feather', help='full seq_info file with description column')
+    p.add_argument(
         'url',
         help='Database string URI or filename.')
     p.add_argument(
         '--out',
         default=sys.stdout,
         type=argparse.FileType('w'),
-        help='list of all version downloaded')
+        help='feather file md5sum [stdout')
     args = p.parse_args()
+    info = pandas.read_feather(args.feather)
     conf = configparser.SafeConfigParser(allow_no_value=True)
     conf.optionxform = str  # options are case-sensitive
     conf.read(args.url)
@@ -31,9 +36,11 @@ def main():
     meta = sqlalchemy.MetaData(engine, reflect=True)
     nodes = meta.tables['nodes']
     s = sqlalchemy.select([nodes.c.tax_id]).where(nodes.c.is_valid)
-    result = conn.execute(s)
-    for i in result:
-        args.out.write(i[0] + '\n')
+    named = set(i[0] for i in conn.execute(s))
+    info['is_valid'] = False
+    info.loc[info['tax_id'].isin(named), 'is_valid'] = True
+    info.to_feather(args.feather)
+    args.out.write(hashlib.md5(open(args.feather, 'rb').read()).hexdigest())
 
 
 if __name__ == '__main__':
