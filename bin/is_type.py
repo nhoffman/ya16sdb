568745e8385ae66dad05b895dfa3eb956623ea01
crosenth <crosenth@uw.edu>
2019-04-25 13:40:54 -0700

    creates is_type column in feather file

--- bin/is_type.py
+++ bin/is_type.py
@@ -3,7 +3,8 @@
 Add is_type column based on presence in type strain file
 """
 import argparse
-import csv
+import hashlib
+import pandas
 import sys
 
 
@@ -12,29 +13,24 @@ def main():
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     p.add_argument(
-        'seq_info',
-        type=argparse.FileType('r'),
-        nargs='?',
-        default=sys.stdin,
-        help='full seq_info file with description column')
+        'feather', help='full seq_info file with description column')
     p.add_argument(
         'types',
         type=argparse.FileType('r'),
         help='txt file of version numbers that are type strains')
     p.add_argument(
         '--out',
-        type=argparse.FileType('w'),
         default=sys.stdout,
-        help='list of all version downloaded')
+        type=argparse.FileType('w'),
+        help='feather file md5sum [stdout]')
     args = p.parse_args()
     types = (t.strip() for t in args.types)
     types = set(t for t in types if t)
-    info = csv.DictReader(args.seq_info)
-    writer = csv.DictWriter(args.out, fieldnames=info.fieldnames + ['is_type'])
-    writer.writeheader()
-    for row in info:
-        row['is_type'] = row['version'] in types
-        writer.writerow(row)
+    info = pandas.read_feather(args.feather)
+    info['is_type'] = False
+    info.loc[info['version'].isin(types), 'is_type'] = True
+    info.to_feather(args.feather)
+    args.out.write(hashlib.md5(open(args.feather, 'rb').read()).hexdigest())
 
 
 if __name__ == '__main__':
