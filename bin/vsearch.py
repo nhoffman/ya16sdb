df93d787df4f2d6f4c1d0a191fcbfed5a10a71da
crosenth <crosenth@uw.edu>
2019-05-14 11:28:21 -0700

    concat new and old unknown records

--- bin/vsearch.py
+++ bin/vsearch.py
@@ -18,9 +18,8 @@ def main():
     p.add_argument(
         'fasta',
         help='vsearch fasta file')
-    p.add_argument(
-        'seq_info',
-        help='seq_info file')
+    p.add_argument('seq_info')
+    p.add_argument('prev_unknowns')
     p.add_argument(
         'out_fa',
         help='fasta output of sequences in forward orientation')
@@ -28,20 +27,20 @@ def main():
         'out_info',
         help='fasta output of sequences in forward orientation')
     p.add_argument(
-        'unknown',
+        'unknowns',
         help=('fasta format output of sequences not '
               'aligned or with invalid sequence characters'))
     p.add_argument(
-        'unknown_accessions',
+        'unknown_versions',
         help=('accession.versions of records that '
               'contain at least one non-16s alignment'))
     args = p.parse_args()
     vsearch = (row.strip().split('\t') for row in open(args.vsearch))
     vsearch = (row for row in vsearch if row[1] != '*')
     vsearch = sorted(vsearch, key=lambda x: x[2], reverse=True)
-    # we want the plus (x[2]) aligns and they will be at the end
+    # we want the plus aligns at the end of x[2]
     vsearch = {row[0]: row[2] for row in vsearch}
-    with open(args.out_fa, 'w') as out, open(args.unknown, 'w') as unknown:
+    with open(args.out_fa, 'w') as out, open(args.unknowns, 'w') as unknowns:
         parsed = SeqIO.parse(args.fasta, 'fasta', Alphabet.IUPAC.ambiguous_dna)
         for s in parsed:
             if s.id in vsearch and Alphabet._verify_alphabet(s.seq):
@@ -49,13 +48,18 @@ def main():
                     s.seq = s.seq.reverse_complement()
                 out.write('>{}\n{}\n'.format(s.description, s.seq))
             else:
-                unknown.write('>{}\n{}\n'.format(s.description, s.seq))
+                unknowns.write('>{}\n{}\n'.format(s.description, s.seq))
     info = pandas.read_csv(args.seq_info, dtype=str)
-    unknown = info[~info['seqname'].isin(vsearch)]
-    unknown['version'].drop_duplicates().to_csv(
-        args.unknown_accessions, header=False, index=False)
     info = info[info['seqname'].isin(vsearch)]
     info.to_csv(args.out_info, index=False)
+    unknowns = info[~info['seqname'].isin(vsearch)]['version'].tolist()
+    with open(args.prev_unknowns, 'r') as unknowns_in:
+        unknowns_in = (u.strip() for u in unknowns_in)
+        unknowns_in = (u for u in unknowns_in if u)
+        unknowns.extend(unknowns_in)
+    with open(args.unknown_versions, 'w') as unknowns_out:
+        for u in set(unknowns):
+            unknowns_out.write(u + '\n')
 
 
 if __name__ == '__main__':
