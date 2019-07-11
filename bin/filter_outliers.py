bf8caa04596e525d5377e592834f05b7e504c2f6
crosenth <crosenth@uw.edu>
2019-07-11 14:23:13 -0700

    adding rank_order and dist_pct columns

--- bin/filter_outliers.py
+++ bin/filter_outliers.py
@@ -33,6 +33,12 @@ def main():
             }
         )
     details['filter_outliers'] = True
+    details['dist_pct'] = details['dist'] * 100
+    # sort values by dist for rank_order column
+    dist_sort = details.sort_values(by='dist')
+    species_groups = dist_sort.groupby(by='species')['species']
+    dist_sort['rank_order'] = species_groups.transform(lambda x: range(len(x)))
+    details = details.merge(dist_sort[['seqname', 'rank_order']])
     info = info.merge(details, how='left')
     info.loc[info['filter_outliers'].isna(), 'filter_outliers'] = False
     info.loc[info['is_out'].isna(), 'is_out'] = False
