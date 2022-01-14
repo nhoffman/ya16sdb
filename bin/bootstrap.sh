66b118718c183b1167c7d47e7506c502213c7624
Chris Rosenthal <crosenth@gmail.com>
2022-01-14 09:44:28 -0800

    do not check if deps exist

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -14,33 +14,22 @@ $venv/bin/pip3 install --requirement requirements.txt
 
 mkdir src
 
-if [[ ! -f $venv/bin/cmsearch ]]; then
-  (cd src &&
-   wget -nc --quiet http://eddylab.org/infernal/infernal-1.1.4-linux-intel-gcc.tar.gz && \
-   tar xvf infernal-1.1.4-linux-intel-gcc.tar.gz --strip-components 2 --directory $venv/bin infernal-1.1.4-linux-intel-gcc/binaries/cmsearch)
-else
-  echo "cmsearch is already installed"
-fi
+INFERNAL_GZ=infernal-1.1.4-linux-intel-gcc.tar.gz
+(cd src &&
+ wget -nc --quiet http://eddylab.org/infernal/$INFERNAL_GZ &&
+ tar xvf $INFERNAL_GZ --strip-components 2 --directory $venv/bin infernal-1.1.4-linux-intel-gcc/binaries/cmsearch)
 
+BLAST_GZ=ncbi-blast-*-x64-linux.tar.gz
+(cd src && \
+ wget --quiet -nc --user anonymous --password anonymous \
+   ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/$BLAST_GZ &&
+ tar tzf $BLAST_GZ | grep makeblastdb | xargs tar xzf $BLAST_GZ --strip-components 2 --directory $venv/bin)
 
-if [[ ! -f $venv/bin/makeblastdb ]]; then
-  BLAST_GZ=ncbi-blast-*-x64-linux.tar.gz
-  (cd src &&
-   wget -nc --user anonymous --password anonymous \
-      ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/$BLAST_GZ &&
-   tar tzf $BLAST_GZ | grep makeblastdb | xargs tar xzf $BLAST_GZ --strip-components 2 --directory $venv/bin)
-else
-  echo "makeblastdb is already installed: $(makeblastdb -version)"
-fi
-
-if [[ ! -f $venv/bin/vsearch ]]; then
-  (cd src &&
-   wget https://github.com/torognes/vsearch/releases/download/v2.13.0/vsearch-2.13.0-linux-x86_64.tar.gz &&
-   tar tzf vsearch-2.13.0-linux-x86_64.tar.gz |
-   grep bin/vsearch | xargs tar xzf vsearch-2.13.0-linux-x86_64.tar.gz --strip-components 2 --directory $venv/bin)
-else
-  echo "vsearch already installed"
-fi
+VSEARCH_GZ=vsearch-2.13.0-linux-x86_64.tar.gz
+(cd src &&
+ wget --quiet https://github.com/torognes/vsearch/releases/download/v2.13.0/$VSEARCH_GZ &&
+ tar tzf $VSEARCH_GZ | \
+ grep bin/vsearch | xargs tar xzf $VSEARCH_GZ --strip-components 2 --directory $venv/bin)
 
 rm -rf src
 
