fafc3b7b3273164f4f2074573a04a4de00b81fae
Chris Rosenthal <crosenth@uw.edu>
2022-04-11 13:26:48 -0700

    pulling correct deenurp version tag

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -18,7 +18,7 @@ $venv/bin/pip3 install --requirement $pipeline_dir/requirements.txt
 mkdir src
 
 (cd src &&
- git clone --branch 0.3.0 https://github.com/fhcrc/deenurp.git  &&
+ git clone --branch v0.3.1 https://github.com/fhcrc/deenurp.git  &&
  cd deenurp &&
  PYTHON=$venv/bin/python3 \
  DEENURP=$(pwd) \
