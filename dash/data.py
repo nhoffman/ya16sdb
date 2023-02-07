887a5127fab7323e71a8558d81ef6c640bf4474c
Noah Hoffman <noah.hoffman@gmail.com>
2023-02-07 13:19:11 -0800

    runs from docker container
    - store unpinned dependencies in requirements-bare.txt
    - update sample data to supply missing field

--- dash/data.py
+++ dash/data.py
@@ -1,13 +1,26 @@
+"""Reads a feather file containing input data for the Dash app.
+
+When executed as a script, downsamples the full data set to a few
+select species.
+
+"""
+
 import datetime
 import gzip
 import io
 import os
-import pandas
+import sys
+import argparse
+import logging
+
+import pandas as pd
 
 try:
     import boto3
 except ImportError:
     pass
+
+log = logging.getLogger(__name__)
 
 
 def read_feather(pathspec, aws_access_key_id=None, aws_secret_access_key=None,
@@ -19,25 +32,25 @@ def read_feather(pathspec, aws_access_key_id=None, aws_secret_access_key=None,
 
     """
 
-    print('reading data from {}'.format(pathspec))
+    log.info('reading data from {}'.format(pathspec))
     if pathspec.startswith('s3://'):
         s3_bucket, s3_key = pathspec.replace('s3://', '').split('/', 1)
         if aws_access_key_id:
-            print('using provided access keys')
+            log.info('using provided access keys')
             s3client = boto3.client(
                 's3',
                 aws_access_key_id=aws_access_key_id,
                 aws_secret_access_key=aws_secret_access_key)
         else:
             # assume credentials are available in default credential chain
-            print('assuming existing aws credentials')
+            log.info('assuming existing aws credentials')
             s3client = boto3.Session(region_name='us-west-2').client('s3')
         s3obj = s3client.get_object(Bucket=s3_bucket, Key=s3_key)
         last_modified = s3obj['LastModified']
         if get_data:
             compressed = io.BytesIO(s3obj['Body'].read())
             with gzip.GzipFile(mode='rb', fileobj=compressed) as f:
-                df = pandas.read_feather(f)
+                df = pd.read_feather(f)
         else:
             df = None
     else:
@@ -46,15 +59,49 @@ def read_feather(pathspec, aws_access_key_id=None, aws_secret_access_key=None,
         if get_data:
             if pathspec.endswith('.gz'):
                 with gzip.open(pathspec) as f:
-                    df = pandas.read_feather(f)
+                    df = pd.read_feather(f)
             else:
-                df = pandas.read_feather(pathspec)
+                df = pd.read_feather(pathspec)
         else:
             df = None
 
     return df, last_modified
 
 
+def main(arguments):
+
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-i', '--infile', default='filter_details.feather.gz')
+    parser.add_argument('-o', '--outfile', help="filtered feather file")
+
+    args = parser.parse_args(arguments)
+
+    data, last_modified = read_feather(args.infile)
+    print(f'last modified: {last_modified}')
+    print(data.describe())
+
+    # 1351: Enterococcus faecalis
+    # 1352: Enterococcus faecium
+    # 851: Fusobacterium nucleatum
+
+    filtered = (data
+                .loc[data['species'].isin(['1351', '851'])]
+                .loc[data['dist_pct'] < 200]
+                .reset_index())
+    print(filtered.describe())
+
+    if outfile := args.outfile:
+        log.info(f'writing {outfile}')
+        if outfile.endswith('.gz'):
+            with gzip.open(outfile, 'wb') as f:
+                filtered.to_feather(f)
+        else:
+            filtered.to_feather(outfile)
+
+
 if __name__ == '__main__':
-    data, __ = read_feather('filter_details.feather.gz')
-    print(data.head())
+    sys.exit(main(sys.argv[1:]))
+
+
---
+++ dash/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+source /usr/local/share/venv/bin/activate
+
+if [[ "$1" = 'run' ]]; then
+    exec gunicorn app:server -b 0.0.0.0:8000 --workers=4 --capture-output --log-file -
+else
+    exec "$@"
+fi
