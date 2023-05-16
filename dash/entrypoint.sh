6de6e21a91389da69c31d482a6e2551150585d79
Noah Hoffman <noah.hoffman@gmail.com>
2023-05-16 11:43:15 -0700

    use a single guincorn worker

--- dash/entrypoint.sh
+++ dash/entrypoint.sh
@@ -6,7 +6,6 @@ source /usr/local/share/venv/bin/activate
 if [[ "$1" = 'run' ]]; then
     exec gunicorn app:server \
          -b 0.0.0.0:8000 \
-         --workers=4 \
          --timeout 600 \
          --capture-output --log-file -
 else
