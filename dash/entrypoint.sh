822f047ad32adbc68138251594486ea1e34bcf0d
Chris Rosenthal <crosenth@uw.edu>
2023-07-10 13:25:05 -0700

    I think this should be ==

--- dash/entrypoint.sh
+++ dash/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 source /usr/local/share/venv/bin/activate
 
-if [[ "$1" = 'run' ]]; then
+if [[ "$1" == 'run' ]]; then
     exec gunicorn app:server \
          -b 0.0.0.0:8000 \
          --timeout 600 \
