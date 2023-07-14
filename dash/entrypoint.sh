99e21969d226dd53e5329afaeb5de03ea0f0f77d
Chris Rosenthal <crosenth@uw.edu>
2023-07-14 10:20:52 -0700

    some extra container logging

--- dash/entrypoint.sh
+++ dash/entrypoint.sh
@@ -7,7 +7,7 @@ if [[ "$1" == 'run' ]]; then
     exec gunicorn app:server \
          -b 0.0.0.0:8000 \
          --timeout 600 \
-         --capture-output --log-file -
+         --capture-output --log-file - & (dmesg | grep gunicorn)
 else
     exec "$@"
 fi
