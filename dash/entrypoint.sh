cb117dc876bf04003a153d6adfb178c2409f8295
Chris Rosenthal <crosenth@uw.edu>
2023-07-17 11:41:52 -0700

    disable timeouts to see if that fixes things

--- dash/entrypoint.sh
+++ dash/entrypoint.sh
@@ -6,8 +6,8 @@ source /usr/local/share/venv/bin/activate
 if [[ "$1" == 'run' ]]; then
     exec gunicorn app:server \
          -b 0.0.0.0:8000 \
-         --timeout 600 \
-         --capture-output --log-file - & (dmesg | grep gunicorn)
+         --timeout 0 \
+         --capture-output --log-file -
 else
     exec "$@"
 fi
