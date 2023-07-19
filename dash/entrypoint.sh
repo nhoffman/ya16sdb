282ebe53a2d63599dbc3faaea848ebe3269bfd82
Chris Rosenthal <crosenth@uw.edu>
2023-07-18 21:06:56 -0700

    Should be able to use default --timeout setting

--- dash/entrypoint.sh
+++ dash/entrypoint.sh
@@ -6,7 +6,6 @@ source /usr/local/share/venv/bin/activate
 if [[ "$1" == 'run' ]]; then
     exec gunicorn app:server \
          -b 0.0.0.0:8000 \
-         --timeout 0 \
          --capture-output --log-file -
 else
     exec "$@"
