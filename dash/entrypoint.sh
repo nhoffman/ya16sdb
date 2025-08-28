3e06e067de3124631c8784e9ac790d8d330f7f8a
Chris Rosenthal <crosenth@uw.edu>
2025-08-27 21:04:45 -0700

    raising timeout limit to account for datetime[ns] dtyping

--- dash/entrypoint.sh
+++ dash/entrypoint.sh
@@ -6,6 +6,7 @@ source /usr/local/share/venv/bin/activate
 if [[ "$1" == 'run' ]]; then
     exec gunicorn app:server \
          -b 0.0.0.0:8000 \
+         -t 240 \
          --capture-output --log-file -
 else
     exec "$@"
